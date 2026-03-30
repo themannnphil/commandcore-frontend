f0cd0be46a16414811a1bc5d12e9d32459b74dae
Prince Phil <princephil403@gmail.com>
2026-03-30 09:24:54 +0000

    fix: next.config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
 }
+const path = require("path");
 
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+};
 module.exports = nextConfig
